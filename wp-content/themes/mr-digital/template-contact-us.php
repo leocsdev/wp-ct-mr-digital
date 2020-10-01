56f165c5d686d4f9a726676b8217c6ef46d38dff
Leo Santillan <leocsdev@gmail.com>
2020-10-01 09:47:37 +0800

    Added template for contact us page.

---
+++ wp-content/themes/mr-digital/template-contact-us.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	Template Name: Contact Us
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+	<h1><?php the_title(); ?></h1>
+
+	<div class="row">
+		<div class="col-md-6">
+			<?php get_template_part('includes/section', 'content') ?>
+		</div>
+	</div>	
+</div>
+
+<?php get_footer(); ?>
