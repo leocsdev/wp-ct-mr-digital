bef90981e4b5e9895c721f155f5889efcb5a5ef9
Leo <leocsdev@gmail.com>
2020-10-21 00:28:44 +0800

    Added some comments how the this file works.

--- wp-content/themes/mr-digital/includes/section-archive.php
+++ wp-content/themes/mr-digital/includes/section-archive.php
@@ -1,3 +1,9 @@
+<!-- 
+  This will be called by category templates like category-blog.php and category-recipes.php
+  
+  This will pull the content from WP blog posts
+ -->
+
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
   <h3><?php the_title(); ?></h3>
--- wp-content/themes/mr-digital/includes/section-content.php
+++ wp-content/themes/mr-digital/includes/section-content.php
@@ -1,4 +1,9 @@
-<!-- this will pull the content from WP -->
+<!-- 
+  This will be called by page templates like template-contact-us.php
+
+	This will pull the page content from WP
+ -->
+
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
 	<?php the_content(); ?>
