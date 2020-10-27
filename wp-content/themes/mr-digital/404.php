1fb4ae84dddb7395b150cf08e673ba02d6191b31
Leo <leocsdev@gmail.com>
2020-10-28 03:25:21 +0800

    Added content on 404 page.

---
+++ wp-content/themes/mr-digital/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="page-wrap">
+  <div class="container">
+
+    <div class="page-not-found">
+      <h1 class="text-align">Page does not exist.</h1>
+
+      <p>Go back to <a href="<?php echo site_url(); ?>">Homepage</a>.</p>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
